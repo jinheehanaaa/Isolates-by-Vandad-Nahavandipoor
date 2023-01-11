4cd6754b741acf6c32fdeb63560a92c1aeb8ccc9
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-10 18:52:55 -0600

    main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +14,33 @@ void main() {
       home: const HomePage(),
     ),
   );
+}
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+
+  const Person({
+    required this.name,
+    required this.age,
+  });
+
+  Person.fromJson(Map<String, dynamic> json)
+      : name = json["name"] as String,
+        age = json["age"] as int;
+}
+
+void _getPersons(SendPort sp) async {
+  const url = 'http://127.0.0.1:5500/apis/people1.json';
+  final persons = await HttpClient()
+      .getUrl(Uri.parse(url))
+      .then((req) => req.close())
+      .then((response) => response.transform(utf8.decoder).join())
+      .then((jsonString) => json.decode(jsonString) as List<dynamic>)
+      .then((json) => json.map((map) => Person.fromJson(map)));
+
+  Isolate.exit(sp, persons);
 }
 
 class HomePage extends StatelessWidget {
